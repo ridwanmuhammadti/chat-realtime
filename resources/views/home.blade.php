63858acad2511c8abf120bfd0b335a45925973c6
Muhammad Ridwan <ridwanmuhammadti@gmail.com>
2020-06-19 13:37:13 +0800

    frontend done

---
+++ resources/views/home.blade.php
@@ -0,0 +1,194 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="user-wrapper">
+                <ul class="users">
+                    <li class="user">
+                        <span class="pending">
+                        1    
+                        </span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://via.placeholder.com/150" alt="" class="media-object">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">John</p>
+                                <p class="email">John@gmail.com</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="user">
+                        <span class="pending">
+                        1    
+                        </span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://via.placeholder.com/150" alt="" class="media-object">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">John</p>
+                                <p class="email">John@gmail.com</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="user">
+                        <span class="pending">
+                        1    
+                        </span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://via.placeholder.com/150" alt="" class="media-object">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">John</p>
+                                <p class="email">John@gmail.com</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="user">
+                        <span class="pending">
+                        1    
+                        </span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://via.placeholder.com/150" alt="" class="media-object">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">John</p>
+                                <p class="email">John@gmail.com</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="user">
+                        <span class="pending">
+                        1    
+                        </span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://via.placeholder.com/150" alt="" class="media-object">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">John</p>
+                                <p class="email">John@gmail.com</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="user">
+                        <span class="pending">
+                        1    
+                        </span>
+
+                        <div class="media">
+                            <div class="media-left">
+                                <img src="https://via.placeholder.com/150" alt="" class="media-object">
+                            </div>
+
+                            <div class="media-body">
+                                <p class="name">John</p>
+                                <p class="email">John@gmail.com</p>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="col-md-8" id="messages">
+            <div class="message-wrapper">
+                <ul class="messages">
+                    <li class="message clearfix">
+                        <div class="sent">
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis laudantium earum odio quod autem !</p>
+                            <p class="date">1 sep, 2020</p>
+                        </div>
+                    </li>
+                    <li class="message clearfix">
+                        <div class="received">
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis laudantium earum odio quod autem !</p>
+                            <p class="date">1 sep, 2020</p>
+                        </div>
+                    </li>
+                    <li class="message clearfix">
+                        <div class="sent">
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis laudantium earum odio quod autem !</p>
+                            <p class="date">1 sep, 2020</p>
+                        </div>
+                    </li>
+                    <li class="message clearfix">
+                        <div class="received">
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis laudantium earum odio quod autem !</p>
+                            <p class="date">1 sep, 2020</p>
+                        </div>
+                    </li>
+                    <li class="message clearfix">
+                        <div class="sent">
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis laudantium earum odio quod autem !</p>
+                            <p class="date">1 sep, 2020</p>
+                        </div>
+                    </li>
+                    <li class="message clearfix">
+                        <div class="received">
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis laudantium earum odio quod autem !</p>
+                            <p class="date">1 sep, 2020</p>
+                        </div>
+                    </li>
+                    <li class="message clearfix">
+                        <div class="sent">
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis laudantium earum odio quod autem !</p>
+                            <p class="date">1 sep, 2020</p>
+                        </div>
+                    </li>
+                    <li class="message clearfix">
+                        <div class="received">
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis laudantium earum odio quod autem !</p>
+                            <p class="date">1 sep, 2020</p>
+                        </div>
+                    </li>
+                    <li class="message clearfix">
+                        <div class="sent">
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis laudantium earum odio quod autem !</p>
+                            <p class="date">1 sep, 2020</p>
+                        </div>
+                    </li>
+                    <li class="message clearfix">
+                        <div class="received">
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis laudantium earum odio quod autem !</p>
+                            <p class="date">1 sep, 2020</p>
+                        </div>
+                    </li>
+                    <li class="message clearfix">
+                        <div class="sent">
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis laudantium earum odio quod autem !</p>
+                            <p class="date">1 sep, 2020</p>
+                        </div>
+                    </li>
+                    <li class="message clearfix">
+                        <div class="received">
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis laudantium earum odio quod autem !</p>
+                            <p class="date">1 sep, 2020</p>
+                        </div>
+                    </li>
+                  
+                </ul>
+            </div>
+
+            <div class="input-text">
+                <input type="text" name="message" class="submit">
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
