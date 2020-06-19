ea1db76592bf5fafdc3a2de62ff69f7c13d4dd85
Muhammad Ridwan <ridwanmuhammadti@gmail.com>
2020-06-19 23:19:43 +0800

    finally chat basic jquery laravel

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use DB;
+use Pusher\Pusher;
+use App\Message;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +28,68 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $users = User::where('id', '!=', Auth::id() )->get();
+
+         // count how many message are unread from the selected user
+         $users = DB::select("select users.id, users.name, users.avatar, users.email, count(is_read) as unread 
+         from users LEFT  JOIN  messages ON users.id = messages.from and is_read = 0 and messages.to = " . Auth::id() . "
+         where users.id != " . Auth::id() . " 
+         group by users.id, users.name, users.avatar, users.email");
+ 
+        return view('home',compact('users'));
+    }
+
+    public function getMessage($user_id)
+    {
+        $my_id = Auth::id();
+
+        
+        // Make read all unread message
+        Message::where(['from' => $user_id, 'to' => $my_id])->update(['is_read' => 1]);
+
+
+
+        // getting all message for selected user
+        // getting those message which is from = Auth::id() and to = user_id Or From = user-id and to Auth::id()
+
+        $messages = Message::where(function($query) use ($user_id, $my_id){
+            $query->where('from',$my_id)->where('to', $user_id);
+        })->orWhere(function ($query) use ($user_id, $my_id){
+            $query->where('from',$user_id)->where('to',$my_id);
+        })->get();
+
+        // dd($messages);
+        return view('messages.index',compact('messages'));
+    }
+
+    public function sendMessage(Request $request){
+        $from = Auth::id();
+        $to = $request->received_id;
+        $message = $request->message;
+
+        // dd($message);
+
+        $data = new Message();
+        $data->from = $from;
+        $data->to = $to;
+        $data->message = $message;
+        $data->is_read = 0; // message unread
+        $data->save();
+
+        // pusher
+        $options = array(
+            'cluster' => 'ap1',
+            'useTLS' => true
+        );
+
+        $pusher = new Pusher(
+            env('PUSHER_APP_KEY'),
+            env('PUSHER_APP_SECRET'),
+            env('PUSHER_APP_ID'),
+            $options
+        );
+
+        $data = ['from' => $from, 'to' => $to]; // sending from and to user id when pressed enter
+        $pusher->trigger('my-channel', 'my-event', $data);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,188 +6,37 @@
         <div class="col-md-4">
             <div class="user-wrapper">
                 <ul class="users">
-                    <li class="user">
-                        <span class="pending">
-                        1    
-                        </span>
+
+                    @foreach ($users as $user)
+                        
+                    <li class="user" id="{{ $user->id }}">
+                       
+                        @if($user->unread)
+                        <span class="pending">{{ $user->unread }}</span>
+                        @endif
+
 
                         <div class="media">
                             <div class="media-left">
-                                <img src="https://via.placeholder.com/150" alt="" class="media-object">
+                                <img src="{{ $user->avatar }}" alt="" class="media-object">
                             </div>
 
                             <div class="media-body">
-                                <p class="name">John</p>
-                                <p class="email">John@gmail.com</p>
+                                <p class="name">{{ $user->name }}</p>
+                                <p class="email">{{ $user->email }}</p>
                             </div>
                         </div>
                     </li>
-                    <li class="user">
-                        <span class="pending">
-                        1    
-                        </span>
 
-                        <div class="media">
-                            <div class="media-left">
-                                <img src="https://via.placeholder.com/150" alt="" class="media-object">
-                            </div>
+                    @endforeach
 
-                            <div class="media-body">
-                                <p class="name">John</p>
-                                <p class="email">John@gmail.com</p>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="user">
-                        <span class="pending">
-                        1    
-                        </span>
-
-                        <div class="media">
-                            <div class="media-left">
-                                <img src="https://via.placeholder.com/150" alt="" class="media-object">
-                            </div>
-
-                            <div class="media-body">
-                                <p class="name">John</p>
-                                <p class="email">John@gmail.com</p>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="user">
-                        <span class="pending">
-                        1    
-                        </span>
-
-                        <div class="media">
-                            <div class="media-left">
-                                <img src="https://via.placeholder.com/150" alt="" class="media-object">
-                            </div>
-
-                            <div class="media-body">
-                                <p class="name">John</p>
-                                <p class="email">John@gmail.com</p>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="user">
-                        <span class="pending">
-                        1    
-                        </span>
-
-                        <div class="media">
-                            <div class="media-left">
-                                <img src="https://via.placeholder.com/150" alt="" class="media-object">
-                            </div>
-
-                            <div class="media-body">
-                                <p class="name">John</p>
-                                <p class="email">John@gmail.com</p>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="user">
-                        <span class="pending">
-                        1    
-                        </span>
-
-                        <div class="media">
-                            <div class="media-left">
-                                <img src="https://via.placeholder.com/150" alt="" class="media-object">
-                            </div>
-
-                            <div class="media-body">
-                                <p class="name">John</p>
-                                <p class="email">John@gmail.com</p>
-                            </div>
-                        </div>
-                    </li>
+                  
                 </ul>
             </div>
         </div>
 
         <div class="col-md-8" id="messages">
-            <div class="message-wrapper">
-                <ul class="messages">
-                    <li class="message clearfix">
-                        <div class="sent">
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis laudantium earum odio quod autem !</p>
-                            <p class="date">1 sep, 2020</p>
-                        </div>
-                    </li>
-                    <li class="message clearfix">
-                        <div class="received">
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis laudantium earum odio quod autem !</p>
-                            <p class="date">1 sep, 2020</p>
-                        </div>
-                    </li>
-                    <li class="message clearfix">
-                        <div class="sent">
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis laudantium earum odio quod autem !</p>
-                            <p class="date">1 sep, 2020</p>
-                        </div>
-                    </li>
-                    <li class="message clearfix">
-                        <div class="received">
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis laudantium earum odio quod autem !</p>
-                            <p class="date">1 sep, 2020</p>
-                        </div>
-                    </li>
-                    <li class="message clearfix">
-                        <div class="sent">
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis laudantium earum odio quod autem !</p>
-                            <p class="date">1 sep, 2020</p>
-                        </div>
-                    </li>
-                    <li class="message clearfix">
-                        <div class="received">
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis laudantium earum odio quod autem !</p>
-                            <p class="date">1 sep, 2020</p>
-                        </div>
-                    </li>
-                    <li class="message clearfix">
-                        <div class="sent">
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis laudantium earum odio quod autem !</p>
-                            <p class="date">1 sep, 2020</p>
-                        </div>
-                    </li>
-                    <li class="message clearfix">
-                        <div class="received">
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis laudantium earum odio quod autem !</p>
-                            <p class="date">1 sep, 2020</p>
-                        </div>
-                    </li>
-                    <li class="message clearfix">
-                        <div class="sent">
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis laudantium earum odio quod autem !</p>
-                            <p class="date">1 sep, 2020</p>
-                        </div>
-                    </li>
-                    <li class="message clearfix">
-                        <div class="received">
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis laudantium earum odio quod autem !</p>
-                            <p class="date">1 sep, 2020</p>
-                        </div>
-                    </li>
-                    <li class="message clearfix">
-                        <div class="sent">
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis laudantium earum odio quod autem !</p>
-                            <p class="date">1 sep, 2020</p>
-                        </div>
-                    </li>
-                    <li class="message clearfix">
-                        <div class="received">
-                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis laudantium earum odio quod autem !</p>
-                            <p class="date">1 sep, 2020</p>
-                        </div>
-                    </li>
-                  
-                </ul>
-            </div>
-
-            <div class="input-text">
-                <input type="text" name="message" class="submit">
-            </div>
+         
         </div>
     </div>
 </div>
